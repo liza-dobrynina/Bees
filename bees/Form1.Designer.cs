af7baebe37212a921758d95856ed588e99c358b5
liza-dobrynina <dea998@gmail.com>
2017-12-06 10:16:04 +0300

    added form

---
+++ bees/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace bees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.worldTimer = new System.Windows.Forms.Timer(this.components);
+            this.count = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.count)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // worldTimer
+            // 
+            this.worldTimer.Enabled = true;
+            this.worldTimer.Tick += new System.EventHandler(this.worldTimer_Tick);
+            // 
+            // count
+            // 
+            this.count.Location = new System.Drawing.Point(516, 43);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(92, 20);
+            this.count.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(513, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество пчел:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 360);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.count);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.count)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer worldTimer;
+        private System.Windows.Forms.NumericUpDown count;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ bees/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bees
+{
+    public partial class Form1 : Form
+    {
+        World world = new World();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+                world.AddObject(new Bees(world, rnd.NextDouble() * world.WorldOptions.WorldWidth, rnd.NextDouble() * world.WorldOptions.WorldHeight));
+        }
+
+        private void worldTimer_Tick(object sender, EventArgs e)
+        {
+            world.Action();
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            world.Draw(e.Graphics);
+        }
+    }
+}
